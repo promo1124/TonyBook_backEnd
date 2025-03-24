58ed727935c47e8fd72f854185a2738cc1838bd9
Johanna Beringuer <mandriciacara@gmail.com>
2025-03-24 14:39:49 +0100

    registerMofif

--- src/Controller/RegistrationFormController.php
+++ src/Controller/RegistrationFormController.php
@@ -16,13 +16,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RegistrationFormController extends AbstractController
 {
-    #[Route('/registration/form', name: 'app_registration_form')]
-    public function index(): Response
-    {
-        return $this->render('registration_form/index.html.twig', [
-            'controller_name' => 'RegistrationFormController',
-        ]);
-    }
+
 
     #[Route('/register', name: 'app_register', methods: ['POST'])]
 
